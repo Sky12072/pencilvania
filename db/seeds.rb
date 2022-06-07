39bc4610dfe1539ca4e9e3b913e5e261156935be
Sky12072 <12072@coderacademy.edu.au>
2022-06-07 10:28:59 +1000

    update controller to permit username

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = []
+categories = ["mechanical", "graphics", "office", "carbon", "shading", "kids", "quill", "coloured" ]
 
 if Category.count == 0
     categories.each do |category|

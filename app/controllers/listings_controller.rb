07313c3b2c505994edb7352dcff88fd5b4ae8556
Sky12072 <12072@coderacademy.edu.au>
2022-06-14 17:12:32 +1000

    put test text on listing_controller so I can push

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,7 +3,7 @@ class ListingsController < ApplicationController
   before_action :set_form_vars, only: %i[ new edit ]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :authorize_user, only: [:edit, :update, :destroy]
-
+  #test
 
   # GET /listings or /listings.json
   def index
